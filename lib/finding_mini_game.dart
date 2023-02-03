c126fc1d4dcde05c2168c758bcc38fe42c4aa935
murilinhops <murilo.pescatori@nubank.com.br>
2023-02-03 16:23:04 -0300

    Create init structur

---
+++ lib/finding_mini_game.dart
@@ -0,0 +1,1 @@
+library finding_mini_game;
---
+++ lib/src/mini_game_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MiniGameWidget extends StatefulWidget {
+  const MiniGameWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MiniGameWidget> createState() => _MiniGameWidgetState();
+}
+
+class _MiniGameWidgetState extends State<MiniGameWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
