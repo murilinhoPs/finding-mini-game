4d90cb7b70310deeab853fb71ddb83737b002b0e
murilinhops <murilo.pescatori@nubank.com.br>
2023-03-15 10:51:12 -0300

    Organize game_canvas_states

--- lib/src/controllers/game_canvas/game_canvas_states.dart
+++ lib/src/controllers/game_canvas/game_canvas_states.dart
@@ -2,39 +2,27 @@ import 'package:equatable/equatable.dart';
 
 abstract class GameCanvasState extends Equatable {
   const GameCanvasState();
+
+  @override
+  List<Object?> get props => [];
 }
 
 class GameCanvasInitial extends GameCanvasState {
   const GameCanvasInitial();
-
-  @override
-  List<Object?> get props => [];
 }
 
 class GameCanvasItemCreatedSuccess extends GameCanvasState {
   const GameCanvasItemCreatedSuccess();
-
-  @override
-  List<Object?> get props => [];
 }
 
 class GameCanvasItemCreatedFailure extends GameCanvasState {
   const GameCanvasItemCreatedFailure();
-
-  @override
-  List<Object?> get props => [];
 }
 
 class GameCanvasCollectibleAddSuccess extends GameCanvasState {
   const GameCanvasCollectibleAddSuccess();
-
-  @override
-  List<Object?> get props => [];
 }
 
 class GameCanvasCollectibleAddFailure extends GameCanvasState {
   const GameCanvasCollectibleAddFailure();
-
-  @override
-  List<Object?> get props => [];
 }
