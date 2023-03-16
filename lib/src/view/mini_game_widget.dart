ebef9c5f8b901120cc6d35a31ab2ec1fe8602596
murilinhops <murilo.pescatori@nubank.com.br>
2023-03-16 17:16:13 -0300

    Add visual debbuger for timerController

--- lib/src/view/mini_game_widget.dart
+++ lib/src/view/mini_game_widget.dart
@@ -1,18 +1,25 @@
 import 'dart:ui' as ui;
 
+import 'package:finding_mini_game/src/controllers/clues/clues_controller.dart';
+import 'package:finding_mini_game/src/controllers/clues/clues_states.dart';
+import 'package:finding_mini_game/src/controllers/timer/ticker.dart';
+import 'package:finding_mini_game/src/controllers/timer/timer_controller.dart';
+import 'package:finding_mini_game/src/controllers/timer/timer_states.dart';
 import 'package:finding_mini_game/src/models/mini_game_data.dart';
 import 'package:flutter/material.dart';
 
 class MiniGameWidget extends StatefulWidget {
-  final String imagePath;
-  final String miniGameJsonpath;
-  final List<String>? imagesPath;
-  const MiniGameWidget({
-    Key? key,
-    required this.imagePath,
-    required this.imagesPath,
-    required this.miniGameJsonpath,
-  }) : super(key: key);
+  const MiniGameWidget({Key? key}) : super(key: key);
+
+  // final String imagePath;
+  // final String miniGameJsonpath;
+  // final List<String>? imagesPath;
+  // const MiniGameWidget({
+  //   Key? key,
+  //   required this.imagePath,
+  //   required this.imagesPath,
+  //   required this.miniGameJsonpath,
+  // }) : super(key: key);
 
   @override
   State<MiniGameWidget> createState() => _MiniGameWidgetState();
@@ -23,8 +30,72 @@ class _MiniGameWidgetState extends State<MiniGameWidget> {
   late ui.Image background;
   late MiniGameDataModel miniGame;
 
+  final CluesController cluesController = CluesController();
+  late TimerController timerController;
+
+  @override
+  void initState() {
+    cluesController.value = const CluesStates(cluesTimeCount: 16);
+    timerController = TimerController(
+        ticker: const Ticker(), cluesController: cluesController);
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Mini-game'),
+        backgroundColor: Colors.white12,
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          timerDebug(),
+        ],
+      )),
+    );
+  }
+
+  Widget timerDebug() {
+    return ValueListenableBuilder(
+      valueListenable: timerController,
+      builder: (_, TimerState state, child) {
+        final duration = timerController.value.duration;
+        final minutesStr =
+            ((duration / 60) % 60).floor().toString().padLeft(2, '0');
+        final secondsStr = (duration % 60).floor().toString().padLeft(2, '0');
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (state is TimerInitial) ...[
+              ElevatedButton(
+                child: const Icon(Icons.play_arrow),
+                onPressed: () => timerController.startTimer(0),
+              ),
+            ],
+            Text(
+              'debugTimer: $minutesStr:$secondsStr',
+              style: const TextStyle(color: Colors.white),
+            ),
+            SizedBox(
+              height: 30,
+              child: FloatingActionButton(
+                backgroundColor:
+                    state is TimerRunComplete ? Colors.lightGreen : Colors.red,
+                child: const Icon(
+                  Icons.restore,
+                  size: 20,
+                ),
+                onPressed: () => timerController.timerReset(),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
