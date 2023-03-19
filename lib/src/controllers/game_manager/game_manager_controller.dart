346565bf1a2323acccc658f60116fec29b271db4
MurilinhoPs <murilinhorps@gmail.com>
2023-03-18 21:52:01 -0300

    Update gameManagerController to get inventoryMaxCapacity

--- lib/src/controllers/game_manager/game_manager_controller.dart
+++ lib/src/controllers/game_manager/game_manager_controller.dart
@@ -1,19 +1,30 @@
 import 'package:finding_mini_game/src/controllers/game_manager/game_manager_states.dart';
 import 'package:finding_mini_game/src/controllers/inventory/inventory_controller.dart';
+import 'package:finding_mini_game/src/models/mini_game_data.dart';
 import 'package:flutter/material.dart';
 
 class GameManagerController extends ValueNotifier<GameManagerState> {
   GameManagerController({
-    required this.inventoryMaxCapacity,
     required this.inventoryController,
+    required this.items,
   }) : super(const GameManagerInitial());
-  final int inventoryMaxCapacity;
+  final List<Item> items;
   final InventoryController inventoryController;
+
+  int _inventoryMaxCapacity = 0;
 
   GameManagerState get state => value;
 
+  void initGameManager() {
+    for (var item in items) {
+      if (item.content != null) {
+        _inventoryMaxCapacity++;
+      }
+    }
+  }
+
   void gameCompleted() {
-    if (inventoryController.collectibles.length == inventoryMaxCapacity) {
+    if (inventoryController.collectibles.length == _inventoryMaxCapacity) {
       value = const GameManagerLevelComplete();
       return;
     }
--- test/controllers/game_manager_controller_test.dart
+++ test/controllers/game_manager_controller_test.dart
@@ -3,13 +3,49 @@ import 'package:finding_mini_game/src/controllers/game_manager/game_manager_stat
 import 'package:finding_mini_game/src/controllers/inventory/inventory_controller.dart';
 import 'package:finding_mini_game/src/controllers/inventory/inventory_entity.dart';
 import 'package:finding_mini_game/src/models/collectible.dart';
+import 'package:finding_mini_game/src/models/mini_game_data.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   late InventoryController inventoryController;
   late GameManagerController gameManagerController;
 
-  const maxInventoryCapacity = 2;
+  final itemsListMock = [
+    Item(
+      id: '1',
+      x: 800.2,
+      y: 40.4,
+      image: 'collectible1_image.png',
+      show: true,
+      content: Content(
+        id: '1:1',
+        type: 'document',
+        text: 'text',
+        name: 'name',
+        image: 'collectible1_image.png',
+      ),
+      requiredState: {
+        'key': true,
+      },
+    ),
+    Item(
+      id: '3',
+      x: 800.2,
+      y: 40.4,
+      image: 'collectible3_image.png',
+      show: true,
+      content: Content(
+        id: '3:3',
+        type: 'document',
+        text: 'text',
+        name: 'name',
+        image: 'collectible1_image.png',
+      ),
+      requiredState: {
+        'key': true,
+      },
+    ),
+  ];
 
   const collectibleListMock = [
     Collectible(
@@ -42,9 +78,11 @@ void main() {
         checkInventoryCapacity: (() {}),
       );
       gameManagerController = GameManagerController(
-        inventoryMaxCapacity: maxInventoryCapacity,
         inventoryController: inventoryController,
+        items: itemsListMock,
       );
+
+      gameManagerController.initGameManager();
     });
 
     test(
